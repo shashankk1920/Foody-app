f1ec0704f8021c3f0519b1855d1740882c3acb7e
Shashank Kumar <136078730+shashankk1920@users.noreply.github.com>
2025-07-12 15:27:14 +0530

    Update generateToken.ts

--- Server/utils/generateToken.ts
+++ Server/utils/generateToken.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { IUser, IUserDocument, User } from '../models/user.model';
+
 import { Response } from 'express';
 
 
@@ -8,6 +8,13 @@ export const generateToken =(res:Response, user:any) => {
     // if furthur it will show the error than we have to see that whether it is working or not than we will change it to IUserDocument
 
     const token = jwt.sign({userId:user._id}, process.env.SECRET_KEY!, {expiresIn:'1d'});
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+});
+
     res.cookie("token", token, {httpOnly:true, sameSite:'strict', maxAge: 86400000});
     return token;
 }       
