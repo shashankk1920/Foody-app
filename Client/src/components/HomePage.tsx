0f6cb62eb270564d84a674d9cf76f57ece08ba55
Shashank Kumar <136078730+shashankk1920@users.noreply.github.com>
2025-07-18 02:37:08 +0530

    Update HomePage.tsx

--- Client/src/components/HomePage.tsx
+++ Client/src/components/HomePage.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "./ui/HeroSection";
 import FeaturedSection from "./FeaturedSection";
+import AdminPromoBanner from "./AdminPromoBanner";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Hero Section */}
       <HeroSection />
+      
+      {/* Admin Promotion Banner */}
+      <AdminPromoBanner />
       
       {/* Featured Restaurants and Dishes Section */}
       <FeaturedSection />
