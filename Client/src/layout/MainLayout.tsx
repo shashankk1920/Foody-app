5ede002f8f9f00c8620aee47e52737cb679604fc
Shashank Kumar <136078730+shashankk1920@users.noreply.github.com>
2025-07-18 02:28:44 +0530

    Update MainLayout.tsx

--- Client/src/layout/MainLayout.tsx
+++ Client/src/layout/MainLayout.tsx
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      <header>
+      {/* Navbar - Fixed positioning */}
+      <header className="fixed top-0 left-0 right-0 z-50">
         <NavBar />
       </header>
-      {/* Main Content */}
-      <main className="flex-grow">
+      {/* Main Content - Add top padding to account for fixed navbar */}
+      <main className="flex-grow pt-16">
         <Outlet />
       </main>
       {/* Footer */}
