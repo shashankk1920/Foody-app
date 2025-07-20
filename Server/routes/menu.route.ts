07e670c43c4108a4ade30aa8d5757b59f0d8100c
Shashank Kumar <136078730+shashankk1920@users.noreply.github.com>
2025-07-20 16:26:58 +0530

    Update menu.route.ts

--- Server/routes/menu.route.ts
+++ Server/routes/menu.route.ts
@@ -3,12 +3,13 @@ import express, { Request, Response } from 'express';
 import upload from '../middlewares/multer';
 
 import { isAuthenticated } from '../middlewares/isAuthenticated';
-import { addMenu, editMenu } from '../controller/menu.controller';
+import { addMenu, editMenu, getMenus } from '../controller/menu.controller';
 
 
 
 const router = express.Router();
 
+router.route("/").get(getMenus); // Public GET route for viewing menus
 router.route("/").post(isAuthenticated, upload.single("image"), addMenu);
 router.route("/:id").put(isAuthenticated, upload.single("image"), editMenu);
  
